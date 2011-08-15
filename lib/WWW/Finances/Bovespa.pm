c07b730cebb05a09376032f2708c7387f708e153
Hernan Lopes <hernan@cpan.org>
2011-08-15 05:36:15 +0000

    Import of HERNAN/WWW-Finances-Bovespa-0.10 from CPAN.
    gitpan-cpan-distribution: WWW-Finances-Bovespa
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         HERNAN/WWW-Finances-Bovespa-0.10.tar.gz
    gitpan-cpan-author:       HERNAN
    gitpan-cpan-maturity:     released

--- lib/WWW/Finances/Bovespa.pm
+++ lib/WWW/Finances/Bovespa.pm
@@ -1,114 +1,188 @@
 package WWW::Finances::Bovespa;
-use strict;
-use warnings;
+use Moose;
 use WWW::Mechanize;
 use XML::XPath;
 
-BEGIN {
-    use Exporter ();
-    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
-    $VERSION     = '0.08';
-    @ISA         = qw(Exporter);
-    #Give a hoot don't pollute, do not export more than needed by default
-    @EXPORT      = qw();
-    @EXPORT_OK   = qw();
-    %EXPORT_TAGS = ();
-}
+our $VERSION = '0.10';
 
 
-=head2 Usage
+has [ qw/descricao codigo ibovespa delay data hora oscilacao valor_ultimo quant_neg mercado bovespa_response/ ] => ( is => 'rw' , isa => 'Str' );
+has [ qw/is_valid/ ] => ( is => 'rw' , isa => 'Int' );
 
- use WWW:Finances::Bovespa
- my $cotacao = WWW::Finances::Bovespa->new( 'PETR3' );
- print $cotacao->{ _codigo };
- print $cotacao->{ _descricao };
- print $cotacao->{ _codigo };
- print $cotacao->{ _ibovespa };
- print $cotacao->{ _delay };
- print $cotacao->{ _data };
- print $cotacao->{ _hora };
- print $cotacao->{ _oscilacao };
- print $cotacao->{ _valor_ultimo };
- print $cotacao->{ _quant_neg };
- print $cotacao->{ _mercado };
-
-
-See Also   : http://www.bovespa.com.br
-
-=cut
-
-sub new
-{
-    
-    my $proto = shift;
-    my $class = ref $proto || $proto;
-    my $self = {};
-    bless $self, $class;
-    my $codigo = shift ; 
-
-    my $base_url = 'http://www.bmfbovespa.com.br/cotacoes2000/formCotacoesMobile.asp?codsocemi=';
-    if ( ! $codigo || $codigo eq '' ) {
+sub find {
+    my ( $self , $args ) = @_;
+    $self->clean;
+    if ( ! $args || ref $args ne 'HASH' || ! exists $args->{ codigo } ) {
+        $self->is_valid( 0 );
         warn "Utilize um código da bovespa para obter resultados.";
         return 0;
     }
+    $self->get_stock_data( $args->{ codigo } );
+    $self->fill_stock_data( ) if $self->is_valid;
+} 
 
-    my $mech = WWW::Mechanize->new();
-    $mech->agent_alias( 'Windows IE 6' );
-    $mech->get( $base_url . $codigo );
-    my $content = $mech->content;
-
-    my $xml = XML::XPath->new( xml => $content );
-
-
-    foreach my $node_html ( $xml->findnodes( '//PAPEL', ) ) {
-        $self->{ _descricao } = $node_html->getAttribute( 'DESCRICAO' );
-        $self->{ _codigo } = $node_html->getAttribute( 'CODIGO' );
-        $self->{ _ibovespa } = $node_html->getAttribute( 'IBOVESPA' );
-        $self->{ _delay } = $node_html->getAttribute( 'DELAY' );
-        $self->{ _data } = $node_html->getAttribute( 'DATA' );
-        $self->{ _hora } = $node_html->getAttribute( 'HORA' );
-        $self->{ _oscilacao } = $node_html->getAttribute( 'OSCILACAO' );
-        $self->{ _valor_ultimo } = $node_html->getAttribute( 'VALOR_ULTIMO' );
-        $self->{ _quant_neg } = $node_html->getAttribute( 'QUANT_NEG' );
-        $self->{ _mercado } = $node_html->getAttribute( 'MERCADO' );
-    }
-
-    return $self;
+sub clean {
+    my ( $self ) = @_; 
+    $self->is_valid( 0 );
+    $self->descricao( '' );
+    $self->codigo( '' ); 
+    $self->ibovespa ( '' );
+    $self->delay ( '' );
+    $self->data ( '' );
+    $self->hora ( '' );
+    $self->oscilacao ( '' );
+    $self->valor_ultimo( '' );
+    $self->quant_neg( '' );
+    $self->mercado( '' );
+    $self->bovespa_response( '' );
+}
+  
+sub get_stock_data {
+  my ( $self, $codigo ) = @_;
+  my $base_url = 'http://www.bmfbovespa.com.br/cotacoes2000/formCotacoesMobile.asp?codsocemi=';
+  my $mech = WWW::Mechanize->new();
+  $mech->agent_alias( 'Windows IE 6' );
+  $mech->get( $base_url . $codigo );
+  my $content = $mech->content;
+  if ( $content =~ m/erros/i ) {
+    $self->is_valid( 0 ) ;
+    return;
+  }
+  if ( $mech->res->is_success ) {
+    $self->is_valid( 1 );
+    $self->bovespa_response( $content ) ;
+    return;
+  }
+  $self->is_valid( 0 );
+}       
+        
+sub fill_stock_data {
+    my ( $self ) = @_;  
+    my $xml = XML::XPath->new( xml => $self->bovespa_response );
+    foreach my $node_html ( $xml->findnodes( '//PAPEL', )->[0] ) {
+        $self->descricao( $node_html->getAttribute( 'DESCRICAO' ) );
+        $self->codigo( $node_html->getAttribute( 'CODIGO' ) );
+        $self->ibovespa( $node_html->getAttribute( 'IBOVESPA' ) );
+        $self->delay( $node_html->getAttribute( 'DELAY' ) );
+        $self->data( $node_html->getAttribute( 'DATA' ) );
+        $self->hora( $node_html->getAttribute( 'HORA' ) );
+        $self->oscilacao( $node_html->getAttribute( 'OSCILACAO' ) );
+        $self->valor_ultimo( $node_html->getAttribute( 'VALOR_ULTIMO' ) );
+        $self->quant_neg( $node_html->getAttribute( 'QUANT_NEG' ) );
+        $self->mercado( $node_html->getAttribute( 'MERCADO' ) );
+    } 
 }
 
+1;
+
+__END__
 
 
 =head1 NAME
 
-  WWW::Finances::Bovespa DEPRECATED! Use WWW::Finances::Bovespa2 instead which is moosified. 
-  
+  WWW::Finances::Bovespa Reads stock options values from bovespa ( with 15 minutes lag )
 
 =head1 SYNOPSIS
 
-  WWW::Finances::Bovespa reads the latest stock options values directly 
-  from BOVESPA.com.br
-  Be aware that these values always have a 15min delay. 
+  use WWW::Finances::Bovespa;
 
+  my $bovespa = WWW::Finances::Bovespa->new();
+  $bovespa->find( { codigo => 'PETR3' } );
+
+  print $bovespa->ibovespa;
+  print $bovespa->valor_ultimo;
+  print $bovespa->quant_neg;
+  print $bovespa->delay;
+  print $bovespa->codigo;
+  print $bovespa->hora;
+  print $bovespa->data;
+  print $bovespa->descricao;
+  print $bovespa->oscilacao;
+  print $bovespa->mercado;
+
+  print $bovespa->is_valid; #always true when the code is found.
+
+=head1 DESCRIPTION
+
+  Documentation for WWW::Finances::Bovespa;
+
+=head1 METHODS
+
+=head2 WWW::Finances::Bovespa->new()
+  
+  Creates a new empty WWW::Finances::Bovespa object.
+  Use WWW::Finances::Bovespa->find( { codigo => 'foo' } ) to retrieve data.
+
+=head2 WWW::Finances::Bovespa->find( { codigo => 'foo' } )
+
+  Attempts to retrieve data from bovespa based on the code/codigo. 
+  To see a list of valid codes, search at http://www.bmfbovespa.com.br .
+
+=head2 is_valid
+
+  Indicates if the code was found is valid.
+
+=head2 ibovespa
+
+  Returns ibovespa
+
+=head2 valor_ultimo
+  
+  Returns last value
+
+=head2 quant_neg
+
+  Returns negociated amount
+
+=head2 delay
+
+  Returns delay
+
+=head2 codigo
+
+  Returns codigo 
+
+=head2 hora
+
+  Returns hora
+
+=head2 data
+
+  Returns date
+
+=head2 descricao
+
+  Returns description
+
+=head2 oscilacao
+
+  Returns oscilation
+
+=head2 mercado
+
+  Returns market name
+  
 =head1 BUGS
 
-  Let me know
+  Please report any... 
 
 =head1 SUPPORT
 
-  Email me
+  Send me a msg
 
 =head1 AUTHOR
 
-  Hernan Lopes
-  CPAN ID: HERNAN
-  HERNAN
-  hernanlopes@gmail.com
+    Hernan Lopes
+    CPAN ID: HERNAN
+    HERNAN
+    hernanlopes@gmail.com
+    -
 
 =head1 COPYRIGHT
 
 This program is free software licensed under the...
 
-  The BSD License
+	The BSD License
 
 The full text of the license can be found in the
 LICENSE file included with this module.
@@ -116,11 +190,7 @@ LICENSE file included with this module.
 
 =head1 SEE ALSO
 
- http://www.bovespa.com.br
+  http://www.bovespa.com.br
 
 =cut
-
-
-
-1;
 
--- t/001_load.t
+++ t/001_load.t
@@ -2,40 +2,55 @@
 
 # t/001_load.t - check module loading and create testing directory
 
-use Test::More tests => 9;
+#use Test::More 'no_plan';
+use Test::More tests => 26;
 
-BEGIN {
+BEGIN { 
     use_ok( 'WWW::Finances::Bovespa' ); 
+    use_ok( 'Moose' );
     use_ok( 'WWW::Mechanize' );
     use_ok( 'XML::XPath' );
 }
 
-my $stock_code = 'PETR3';
-my $object = WWW::Finances::Bovespa->new ( $stock_code );
-isa_ok ( $object, 'WWW::Finances::Bovespa' );
+my $bovespa2 = WWW::Finances::Bovespa->new();
+$bovespa2->find( { codigo => 'PETR3' } );
+isa_ok ( $bovespa2, 'WWW::Finances::Bovespa' );
+is( 1, $bovespa2->is_valid , 'valid response' );
+is( 1, $bovespa2->ibovespa =~ m/(.+)/ , '' );
+is( 1, $bovespa2->valor_ultimo =~ m/(.+)/ , '' );
+is( 1, $bovespa2->quant_neg =~ m/(.+)/ , '' );
+is( 1, $bovespa2->delay =~ m/(.+)/ , '' );
+is( 1, $bovespa2->codigo =~ m/(.+)/ , '' );
+is( 1, $bovespa2->codigo =~ m/PETR3/ , 'Correct code' );
+is( 1, $bovespa2->hora =~ m/(.+)/ , '' );
+is( 1, $bovespa2->data =~ m/(.+)/ , '' );
+is( 1, $bovespa2->descricao =~ m/(.+)/ , '' );
+is( 1, $bovespa2->descricao =~ m/PETROBRAS/ , 'correct description' );
+is( 1, $bovespa2->oscilacao =~ m/(.+)/ , '' );
+is( 1, $bovespa2->mercado =~ m/(.+)/ , '' );
 
-is ( defined $object->{ _codigo } , 1 , 'is defined' );
-my $is_match = $object->{ _codigo } =~ m/$stock_code$/;
-is ( 1 , $is_match , 'verify if the it has found any code' );
-is ( valid_bovespa_hash_v1( $object ), 1, 'validate the hash result for v1' );
 
-my $invalid_code = '_39';
-my $object2 = WWW::Finances::Bovespa->new ( $invalid_code );
-isnt ( $object2, 'WWW::Finances::Bovespa' );
-is ( valid_bovespa_hash_v1( $object2 ), 0, 'validate the hash result for v1' );
 
-sub valid_bovespa_hash_v1 {
-    my ( $bovespa ) = @_;
-    return 0 if ! exists $bovespa->{ _descricao };
-    return 0 if ! exists $bovespa->{ _codigo };
-    return 0 if ! exists $bovespa->{ _ibovespa };
-    return 0 if ! exists $bovespa->{ _delay };
-    return 0 if ! exists $bovespa->{ _data };
-    return 0 if ! exists $bovespa->{ _hora };
-    return 0 if ! exists $bovespa->{ _oscilacao };
-    return 0 if ! exists $bovespa->{ _valor_ultimo };
-    return 0 if ! exists $bovespa->{ _quant_neg };
-    return 0 if ! exists $bovespa->{ _mercado };
-    return 1;
-}
 
+
+$bovespa2->find( { codigo => 'NONEXISTANT' } );
+isa_ok ( $bovespa2, 'WWW::Finances::Bovespa' );
+is( 0, $bovespa2->is_valid , 'Invalid Code' );
+
+
+
+
+my $bov = WWW::Finances::Bovespa->new();
+$bov->find( { codigo => 'USIM5F' } );
+isa_ok( $bov, 'WWW::Finances::Bovespa' );
+is( 1, $bov->is_valid , 'valid response' );
+is( 1, $bov->ibovespa =~ m/(.+)/ , '' );
+is( 1, $bov->valor_ultimo =~ m/(.+)/ , '' );
+is( 1, $bov->quant_neg =~ m/(.+)/ , '' );
+
+
+
+
+
+$bov->find( 'x' );
+is( 0, $bov->is_valid , 'invalid find' );
